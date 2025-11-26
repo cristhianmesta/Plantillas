b9e866670825aa2d9ff6e947acc560af0d520a65
Cristhian Mesta <cristhianmesta@hotmail.com>
2025-11-25 23:00:55 -0500

    CleanArchitecture: Se agrega configuración de Endpoints

---
+++ CleanArchitecture/src/CleanArchitecture.Api/Extensions/EndpointRegistrationExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+
+namespace CleanArchitecture.Api.Extensions;
+
+public static class EndpointRegistrationExtensions
+{
+    public static void AutoRegisterEndpoints(this WebApplication app)
+    {
+        // Scan all assemblies loaded for types in the namespace "MinimalApiDemo.Endpoints"
+        var endpointTypes = Assembly.GetExecutingAssembly()
+            .GetTypes()
+            .Where(t => t.IsClass && t.IsAbstract && t.IsSealed) // static class
+            .Where(t => t.Namespace != null && t.Namespace.Contains("Endpoints"));
+
+        foreach (var type in endpointTypes)
+        {
+            // Busca método 'MapEndpoints'
+            var method = type.GetMethod("MapEndpoints", BindingFlags.Public | BindingFlags.Static);
+
+            if (method != null)
+            {
+                // Invoca 'MapEndpoints' pasando app como parámetro
+                method.Invoke(null, [app]);
+            }
+        }
+    }
+}
